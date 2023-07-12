26735be86329a01c3d55cde36a85d65b08a2ca58
87102 <davifernando_10@hotmail.com>
2023-07-12 13:59:33 -0300

    projeto criado

---
+++ src/test/java/com/davi/marcelino/cursoSpring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.marcelino.cursoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
