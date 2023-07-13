ecde9aa621d8eaa8583e9683ff2028ef27ec395d
87102 <davifernando_10@hotmail.com>
2023-07-13 15:21:43 -0300

    Payment, one-to-one association

--- src/main/java/com/davi/marcelino/cursoSpring/config/TestConfig.java
+++ src/main/java/com/davi/marcelino/cursoSpring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.davi.marcelino.cursoSpring.entities.Category;
 import com.davi.marcelino.cursoSpring.entities.Order;
 import com.davi.marcelino.cursoSpring.entities.OrderItem;
+import com.davi.marcelino.cursoSpring.entities.Payment;
 import com.davi.marcelino.cursoSpring.entities.Product;
 import com.davi.marcelino.cursoSpring.entities.User;
 import com.davi.marcelino.cursoSpring.entities.enums.OrderStatus;
@@ -80,8 +81,13 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-	}
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	
 	
-	
+}
 }
