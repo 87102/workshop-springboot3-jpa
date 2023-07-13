42f2c25673aaad52557bba6da445bb9d3be8be24
87102 <davifernando_10@hotmail.com>
2023-07-13 15:45:32 -0300

    Subtotal & Total methods

--- src/main/java/com/davi/marcelino/cursoSpring/entities/OrderItem.java
+++ src/main/java/com/davi/marcelino/cursoSpring/entities/OrderItem.java
@@ -1,7 +1,6 @@
 package com.davi.marcelino.cursoSpring.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import com.davi.marcelino.cursoSpring.entities.pk.OrderItemPK;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -17,12 +16,11 @@ public class OrderItem implements Serializable {
 
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
-		
 	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
@@ -32,21 +30,20 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -66,12 +63,17 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-	
-	
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -83,9 +85,11 @@ public class OrderItem implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		OrderItem other = (OrderItem) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	
 }
